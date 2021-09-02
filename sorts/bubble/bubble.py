1e5d743cc03d13afcad79d4a65711cb4e78ba57f
javiluli <javiluli.dev@gmail.com>
2021-09-02 22:56:39 +0200

    se ha corregido un error en la animacion del bubble-sort

--- sorts/bubble/bubble.py
+++ sorts/bubble/bubble.py
@@ -5,9 +5,9 @@ def bubble_sort(data, drawData=None, timeTick=0):
         for j in range(len(data) - 1):
             if data[j] > data[j+1]:
                 data[j], data[j+1] = data[j+1], data[j]
-                if drawData != None:
-                    drawData(data, ['limegreen' if x == j + 1 else "steelblue" for x in range(len(data))])
-                time.sleep(timeTick)
+            if drawData != None:
+                drawData(data, ['limegreen' if x == j + 1 else "steelblue" for x in range(len(data))])
+            time.sleep(timeTick)
     if drawData != None:
         drawData(data, ['limegreen' for x in range(len(data))])
     return data
